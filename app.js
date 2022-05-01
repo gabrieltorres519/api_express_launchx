d0757040d9813d1be1ef24b16612d6b48882c11a
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-04-30 21:24:30 -0500

    Creación de endpoint que probé en Postman con una petición get a la url /v1/explorers

--- app.js
+++ app.js
@@ -17,3 +17,35 @@ app.listen(port,()=>{
 
 
 
+// Creación de endpoint que requese una lista de explorers
+//Utilizamos el método GET de express para este enpoint
+app.get('/v1/explorers',(req,res)=>{
+    console.log(`Api explorers GET ALL requests ${new Date()}`);
+
+
+
+    // Crearemos unos objetos y un array o "lista" para retornarlos
+    const explorer1 = {
+        id: 1,
+        name: 'Gabriel Torres',
+    }
+    const explorer2 = {
+        id: 2,
+        name: 'Gabriel Torres 2',
+    }
+    const explorer3 = {
+        id: 3,
+        name: 'Gabriel Torres 3',
+    }
+    const explorer4 = {
+        id: 4,
+        name: 'Gabriel Torres 4',
+    }
+
+    const explorers = [explorer1,explorer2,explorer3,explorer4]
+
+
+    //Se agrega un código simbólico en la respuesta para que el usuario sepa que todo salió bien
+    res.status(200).json(explorers);
+
+})
